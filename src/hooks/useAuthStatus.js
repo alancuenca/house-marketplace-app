37941fd3616c2c1b87ffbe561c53bbc36fdeb633
Alan Cuenca <alan.r.cuenca@gmail.com>
2022-05-10 20:45:20 -0700

    firebase check authStatus

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react"
+import { getAuth, onAuthStateChange } from "firebase/auth"
+
+export const useAuthStatus = () => {
+    const [loggedIn, setLoggedIn] = useState(false)
+    const [checkingStatus, setCheckingStatus] = useState(true)
+
+    useEffect(() => {
+        const auth = getAuth()
+        onAuthStateChange(auth, (user) => {
+            if (user) {
+                setLoggedIn(true)
+            }
+            setCheckingStatus(false)
+        })
+    })
+    return (
+        { loggedIn, checkingStatus }
+    )
+}
