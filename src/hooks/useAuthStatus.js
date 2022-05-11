0c4da395c8c23bba92095107036cfae25cb8d1bd
Alan Cuenca <alan.r.cuenca@gmail.com>
2022-05-10 21:02:32 -0700

    finish useAuthStatus and add useRef

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,20 +1,24 @@
-import { useEffect, useState } from "react"
-import { getAuth, onAuthStateChange } from "firebase/auth"
+import { useEffect, useState, useRef } from "react"
+import { getAuth, onAuthStateChanged } from "firebase/auth"
 
 export const useAuthStatus = () => {
     const [loggedIn, setLoggedIn] = useState(false)
     const [checkingStatus, setCheckingStatus] = useState(true)
+    const isMounted = useRef(true)
 
     useEffect(() => {
-        const auth = getAuth()
-        onAuthStateChange(auth, (user) => {
-            if (user) {
-                setLoggedIn(true)
-            }
-            setCheckingStatus(false)
-        })
-    })
-    return (
-        { loggedIn, checkingStatus }
-    )
+        if (isMounted) {
+            const auth = getAuth()
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                    setLoggedIn(true)
+                }
+                setCheckingStatus(false)
+            })
+        }
+        return () => {
+            isMounted.current = false
+        }
+    }, [isMounted])
+    return { loggedIn, checkingStatus }
 }
