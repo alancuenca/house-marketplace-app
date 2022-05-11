0e990553ee9a4a61652b6683fd44c87896f67f13
Alan Cuenca <alan.r.cuenca@gmail.com>
2022-05-10 21:52:13 -0700

    import OAuth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,7 +5,6 @@ import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import OAuth from "../components/OAuth"
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
-import OAuth from "../components/OAuth"
 
 function SignIn() {
     const [showPassword, setShowPassword] = useState(false)
