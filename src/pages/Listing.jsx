59601bffdbef6900dc92f1073a484af41d96d1d9
Alan Cuenca <alan.r.cuenca@gmail.com>
2022-05-18 21:49:04 -0700

    display basic listing info

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -31,9 +31,72 @@ function Listing() {
         fetchListing()
     }, [navigate, params.listingId])
 
+    if (loading) {
+        return <Spinner />
+    }
 
-  return (
-    <div>Listing</div>
-  )
+    return (
+        <main>
+            {/* Slide show */}
+
+            <div
+                className="shareIconDiv"
+                onClick={() => {
+                    navigator.clipboard.writeText(window.location.href)
+                    setSharedLinkCopied(true)
+                    setTimeout(() => {
+                        setSharedLinkCopied(false)
+                    }, 2000)
+                }}
+            >
+                <img src={shareIcon} alt="shareIcon" />
+            </div>
+            {shareLinkCopied && <p className='linkCopied'>Link Copied!</p>}
+
+            <div className="listingDetails">
+                <p className="listingName">
+                    {listing.name} -
+                    ${listing.offer
+                        ? listing.discountedPrice
+                            .toString()
+                            .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                        : listing.regularPrice
+                            .toString()
+                            .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                    }
+                </p>
+                <p className="listingLocation">{listing.location}</p>
+                <p className="listingType">
+                    For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+                </p>
+                {listing.offer && (
+                    <p className="discountPrice">
+                        ${listing.regularPrice - listing.discountedPrice} discount
+                    </p>
+                )}
+
+                <ul className="listingDetailsList">
+                    <li>
+                        {listing.bedrooms > 1
+                            ? `${listing.bedrooms} Bedrooms`
+                            : '1 Bathroom'}
+                    </li>
+                    <li>
+                        {listing.bathrooms > 1
+                            ? `${listing.bathrooms} Bathrooms`
+                            : '1 Bathroom'}
+                    </li>
+                    <li>{listing.parking && 'Vehicle Parking'}</li>
+                    <li>{listing.furnished && 'Furnished'}</li>
+                </ul>
+
+                <p className="listingLocationTitle">Location</p>
+
+                <div className="leafletContainer">
+                    
+                </div>
+            </div>
+        </main>
+    )
 }
 export default Listing
